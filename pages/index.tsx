6d942f9ca0fa35a44b99b269f426348bcabc98aa
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-01-27 21:57:33 -0300

    fixed reusable components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import Footer from "../components/footer";
-import Header from "../components/header";
 import Homepage from "../components/homepage";
 
 export default function Home() {
   return (
     <>
-    <Header />
     <Homepage />
-    <Footer />
     </>
   )
 }

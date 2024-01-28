df70227db82dda3fc1713556e4d5c946f6eda90f
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2024-01-27 22:49:23 -0300

    adding hover logo

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from 'react'
 import * as Styled from './styles'
 
-interface ILogoProps {
+export interface ILogoProps {
   logo_text: string
+  isOpen?: boolean
 }
 
 const Logo = ({ logo_text }: ILogoProps) => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(false)
+    }, 5000)
+
+    // return () => clearTimeout()
+  }, [])
+
   return (
-    <Styled.Logo>
+    <Styled.Logo isOpen={isOpen}>
       <img src="/images/white-pokeball.svg" alt="Pokebola" />
       <p>{logo_text}</p>
     </Styled.Logo>
--- components/logo/styles.ts
+++ components/logo/styles.ts
@@ -1,18 +1,39 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { ILogoProps } from './index'
 
-export const Logo = styled.div`
+export const Logo = styled.div<Pick<ILogoProps, 'isOpen'>>`
+  ${($props) =>
+    $props.isOpen &&
+    css`
+      width: 16.1875rem;
+      p {
+        display: block;
+      }
+    `}
+
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: ${({ theme }) => theme.colors.red};
   color: ${({ theme }) => theme.colors.white};
   border-radius: ${({ theme }) => theme.border.regular_radius};
-  width: 16.1875rem;
+  width: fit-content;
+  padding: 0.87rem;
   height: 3.8125rem;
 
   p {
+    display: none;
     margin-left: 1.19rem;
     font-size: 1.25rem;
     font-weight: 600;
+  }
+
+  &:hover {
+    cursor: pointer;
+    width: 16.1875rem;
+    transition: width 2s linear 3s;
+    p {
+      display: block;
+    }
   }
 `
